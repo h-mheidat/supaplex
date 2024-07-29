fb827bb0634be0006063374207abb7b050dd0846
hala811 <you@example.com>
2024-07-29 19:26:34 +0300

    hala

---
+++ hala.py
@@ -0,0 +1,10 @@
+def build_x (height)
+array=[]
+max=(array.len-1)
+for index in 
+reversed (range(len(height))):
+build_height=height[index]
+if height > max
+array.append(len(height))-(index)
+max=buid_height
+return array
