c7a69ee213e96c3cccd69a445453ff985ae45173
dragoescobar <dev@faisal-alnabulsi-linux.scoiumtech.local>
2024-07-29 19:00:04 +0300

    faisal and wadi3

--- print_hamza.py
+++ print_hamza.py
@@ -1,3 +1,13 @@
-
-def print_tamara() -> None:
-    print("Hello Tamara")
+x = [3,5,4,4,3,1,3,2]
+x.reverse()
+max=0
+output=[]
+#[2,3,1,3,4,4,5,3]
+i=0
+for  i in range(len(x)) :
+    if x[i] >max:
+        output.append(x[i])
+        max = x[i] 
+    i +=1
+output.reverse()
+print(output)
