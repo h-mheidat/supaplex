69234a71c409fee27e8f572469273308ade365fc
dev3 <dev@dev-3-linux.scoiumtech.local>
2024-07-29 12:22:33 -0400

    wajed

---
+++ wajed_task.py
@@ -0,0 +1,10 @@
+def building(height):
+    arr=[]
+    max=(arr.len-1)
+    for index in 
+    reversed(range(len(height))):
+    b_height=height[index]
+     if height > max
+     arr.append(len(height))-(index)
+        max =b_height 
+         return arr
