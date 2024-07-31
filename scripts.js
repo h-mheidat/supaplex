32c4d362c27368b2b37ee76d1117044351c5e3aa
Mohammad-Azzawi <Mohammad.azzawi@sociumtech.com>
2024-07-31 16:56:03 +0300

    html task

---
+++ scripts.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('.section button');
+
+    buttons.forEach(button => {
+        const parentSection = button.closest('.section');
+        const bgColor = window.getComputedStyle(parentSection).backgroundColor;
+
+        button.addEventListener('mouseover', function() {
+            button.style.backgroundColor = '#FFFFFF';
+            button.style.color = bgColor;
+            button.style.fontWeight = 'bold'
+            
+        });
+
+        button.addEventListener('mouseout', function() {
+            button.style.backgroundColor = 'transparent';
+            button.style.color = '#FFFFFF';
+            button.style.fontWeight = 'normal';
+        });
+    });
+});
